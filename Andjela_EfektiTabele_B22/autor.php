ecb5eeccda5af6ed650d2ca99b9cc2122a29c523
andjelabekic-afk <andjelabekic@gymko.edu.rs>
2026-04-21 16:58:00 +0200

    Add files via upload

---
+++ Andjela_EfektiTabele_B22/autor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./slike/LOGO.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/autor.css">
+    <title>B22 - Efekti Tabele | Autor</title>
+</head>
+<body>
+    <header>
+        <h1>Efekti Tabele | Autor</h1>
+     
+    </header>
+    <main>
+        <div class="info">
+            <div class="name">
+                <h2>Andjela Bekic</h2>
+                <h4>IV<span>3</span></h4>
+            </div>
+        </div>
+        <div class="contact">
+    <p>Email: andjela.bekic2007@gmail.com</p>
+    <p>Telefon: +381 63 111 111</p>
+    <p>Adresa: Ulica 123, Kovacica, Srbija</p>
+</div>
+    </main>
+       <?php 
+            $url = "autor";
+            require "./components/footer.php";
+        ?>
+</body>
+</html>
