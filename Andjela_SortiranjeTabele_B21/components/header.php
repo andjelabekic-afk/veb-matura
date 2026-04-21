f5e1cd73d6ae4623f1ec9293e066ce177816a8d8
andjelabekic-afk <andjelabekic@gymko.edu.rs>
2026-04-21 16:49:15 +0200

    Add files via upload

---
+++ Andjela_SortiranjeTabele_B21/components/header.php
@@ -0,0 +1,4 @@
+<header>
+    <p>Zadatak 21</p>
+    <p>Sortiranje tabele</p>
+</header>
---
+++ Andjela_SortiranjeTabele_B21/uputstvo.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./slike/LOGO.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/uputstva.css">
+    <title>B1 - Sortiranje Tabele | Uputstvo</title>
+</head>
+<body>
+    <header>
+        <h1>Sortiranje Tabele | Uputstvo</h1>
+  
+    </header>
+    <main>
+   <section>
+    <h2>Početna</h2>
+    <p>
+        Na početnoj stranici možete videti tabelu sa podacima. <br>
+        Tabela sadrži kolone Rbr, Ime i Prezime. <br>
+        Klikom na naziv željene kolone vrši se sortiranje podataka u rastućem redosledu, dok se ponovnim klikom vrši sortiranje u opadajućem redosledu.
+    </p>
+</section>
+<section>
+    <h2>Autor</h2>
+    <p>
+        Na stranici o autoru možete videti podatke o autoru aplikacije, naziv aplikacije i datum kreiranja.
+    </p>
+</section>
+<section>
+    <h2>Uputstvo</h2>
+    <p>
+        Na stranici uputstvo možete videti objašnjenje načina korišćenja aplikacije i njenih funkcionalnosti.
+    </p>
+</section>
+
+   
+    </main>
+         <footer>      <?php 
+            $url = "uputstvo";
+            require "./components/footer.php";
+        ?></footer>
+</body>
+</html>
