fe2ef7a2382bfe1eb58fcc74be0990896a68c797
Andjela <andjelabekic@gymko.edu.rs>
2026-06-04 17:59:27 +0200

    Update zivotinja.php

--- Andjela_DomaceZivotinje_B2/zivotinja.php
+++ Andjela_DomaceZivotinje_B2/zivotinja.php
@@ -1,24 +1,24 @@
 <?php 
 	$zivotinje = array(
 		"pas" => array(
-			"naslov" => "Pas",
-			"opis" => "Pas (lat. Canis lupus familiaris) je pripitomljeni sisar iz porodice Canidae. Arheološki nalazi dokazuju da pas najmanje 9000 godina živi uz čoveka, iako postoje indicije da se taj period produži na čitavih 14.000 godina. Fosilni ostaci pokazuju da su preci modernog psa živeli uz čoveka još pre 10.000 godina."
+			"naslov" => "Пас",
+			"opis" => "Пас (лат. Canis lupus familiaris) је припитомљени сисар из породице Canidae. Археолошки налази доказују да пас најмање 9000 година живи уз човека, иако постоје индиције да се тај период продужи на читавих 14.000 година. Фосилни остаци показују да су преци модерног пса живели уз човека још пре 10.000 година."
 		),
 		"patka" => array(
-			"naslov" => "Patka",
-			"opis" => "Patka je opšti naziv za veliki broj vrsta ptica iz familije Anatidae koja uključuje i labudove i guske. Patke su podeljenje u više podfamilija u okviru familije Anatidae, one nisu monofilična grupa (grupa potomaka koja ima jednog zajedničkog pretka) već čine takson pošto se labudovi i guske ne smatraju patkama. Patke su uglavnom vodene ptice uglavnom manje od labudova i guski i mogu se naći u rečnim i morskim vodama."
+			"naslov" => "Патка",
+			"opis" => "Патка је општи назив за велики број врста птица из фамилије Anatidae, која укључује и лабудове и гуске. Патке су углавном водене птице, мање су од лабудова и гусака и могу се наћи у речним и морским водама."
 		),
 		"macka" => array(
-			"naslov" => "Mačka",
-			"opis" => "Mačka, takođe zvana i domaća mačka ili kućna mačka (lat. Felis catus), mali je mesožder, vrsta sisara iz roda Felis. Veruje se da je njen predak bila afrička divlja mačka (Felis silvestris lybica). Mačke žive u bliskoj vezi sa ljudima najmanje 9.500 godina."
+			"naslov" => "Мачка",
+			"opis" => "Мачка, такође звана и домаћа мачка или кућна мачка (лат. Felis catus), мали је месождер и врста сисара из рода Felis. Верује се да је њен предак била афричка дивља мачка (Felis silvestris lybica). Мачке живе у блиској вези са људима најмање 9.500 година."
 		),
 		"kokoska" => array(
-			"naslov" => "Kokoška",
-			"opis" => "Kokoška, kokoš ili domaća kokoš (lat. Gallus gallus domesticus) je podvrsta ptice koja se često gaji kao živina. Smatra se da je poreklom iz jugoistočne Azije, i da je evoluirala od divljih podvrsta vrste Gallus gallus. Ova ptica je najrasprostranjenija na zemlji, sa totalnom populacijom od preko 19 milijardi po proceni iz 2011. godine. Ljudi uzgajaju kokoške prvenstveno kao izvor hrane, konzumirajući njihovo meso i jaja."
+			"naslov" => "Кокошка",
+			"opis" => "Кокошка, кокош или домаћа кокош (лат. Gallus gallus domesticus) је подврста птице која се често гаји као живина. Сматра се да је пореклом из југоисточне Азије и да је еволуирала од дивљих подврста врсте Gallus gallus. Људи узгајају кокошке првенствено као извор хране, користећи њихово месо и јаја."
 		),
 		"krava" => array(
-			"naslov" => "Krava",
-			"opis" => "Krava je odrasla ženka. Mliječna krava se uzgaja zbog proizvodnje mlijeka, koje se prerađuje u brojne prehrambene proizvode: konzumno mlijeko, pavlaka, kajmak, razne vrste sireva, puter i slično. Domesticiranje je počelo prvo radi mesa, a kasnije radi mlijeka i njihove snage korištene za vuču. Mladunče krave se zove tele."
+			"naslov" => "Крава",
+			"opis" => "Крава је одрасла женка говечета. Млечна крава се узгаја због производње млека, које се прерађује у бројне прехрамбене производе: млеко, павлаку, кајмак, разне врсте сирева, путер и слично. Припитомљавање говеда почело је најпре ради меса, а касније и ради млека и снаге коришћене за вучу. Младунче краве зове се теле."
 		),
 	);
 
@@ -33,7 +33,7 @@
 	}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sr">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
