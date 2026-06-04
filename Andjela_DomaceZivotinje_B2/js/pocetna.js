517fc2d0764253a7f227b11e08fa542221fce023
Andjela <andjelabekic@gymko.edu.rs>
2026-06-04 17:57:58 +0200

    Update pocetna.js

--- Andjela_DomaceZivotinje_B2/js/pocetna.js
+++ Andjela_DomaceZivotinje_B2/js/pocetna.js
@@ -1,15 +1,13 @@
 Array.from(document.querySelectorAll(".image img")).forEach(img => {
 
-    
     img.addEventListener("click", () => {
         window.open(
             "zivotinja.php?zivotinja=" + img.id,
-            "Domaće životinje",
+            "Домаће животиње",
             "width=500,height=300,left=100,top=100,menubar=no,toolbar=no"
         );
     });
 
-    
     img.addEventListener("mouseover", () => {
         let audio = document.getElementById("audio-" + img.id);
         
