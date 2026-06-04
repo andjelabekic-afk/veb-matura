73296798a4a33ca65991438562fb8b4c38131b41
Andjela <andjelabekic@gymko.edu.rs>
2026-06-04 17:56:20 +0200

    Update nav.php

--- Andjela_DomaceZivotinje_B2/components/nav.php
+++ Andjela_DomaceZivotinje_B2/components/nav.php
@@ -3,9 +3,9 @@
 <body>
     <nav>
         <div class="links">
-            <a href="." class="link <?php echo ($url == "index" ? "active" : "") ?>">Početna</a>
-            <a href="./autor.php" class="link <?php echo ($url == "autor" ? "active" : "") ?>">O autoru</a>
-            <a href="./uputstvo.php" class="link <?php echo ($url == "uputstvo" ? "active" : "") ?>">Uputstvo</a>
+            <a href="." class="link <?php echo ($url == "index" ? "active" : "") ?>">Почетна</a>
+            <a href="./autor.php" class="link <?php echo ($url == "autor" ? "active" : "") ?>">О аутору</a>
+            <a href="./uputstvo.php" class="link <?php echo ($url == "uputstvo" ? "active" : "") ?>">Упутство</a>
         </div>
     </nav>
 </body>
