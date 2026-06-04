31873941ea39b5b2924ce3ca316c4968cf9da9cd
Andjela <andjelabekic@gymko.edu.rs>
2026-06-04 17:27:35 +0200

    Update index.php

--- Andjela_FotoGalerija_B1/index.php
+++ Andjela_FotoGalerija_B1/index.php
@@ -7,11 +7,11 @@
     <link rel="shortcut icon" href="./slike/LOGO.ico" type="image/x-icon">
     <link rel="stylesheet" href="./css/global.css">
     <link rel="stylesheet" href="./css/pocetna.css">
-    <title>B1 - Foto Galerija</title>
+    <title>Б1 - Фото галерија</title>
 </head>
 <body>
     <header>
-        <h1>Foto galerija</h1>
+        <h1>Фото галерија</h1>
         <?php 
             $url = "index";
             require "./components/nav.php";
@@ -19,21 +19,21 @@
     </header>
     <main class="gallery-container">
         <div class="gallery">
-            <textarea readonly>Mapa prikazuje raspored sedam svetskih čuda antičkog sveta. Ona pomaže korisniku da vidi gde su se nalazila najpoznatija graditeljska i umetnička dostignuća starog veka. Čuda antičkog sveta pokazuju moć, znanje, bogatstvo i veštinu starih civilizacija.</textarea>
+            <textarea readonly>Мапа приказује распоред седам светских чуда античког света и места на којима су се она налазила. На њој можемо видети да су чуда била распоређена у различитим деловима старог света, од Египта и Месопотамије до Мале Азије и Грчке. Ова мапа помаже кориснику да лакше повеже свако чудо са његовим географским положајем и историјским окружењем. Седам светских чуда античког света представљају најпознатија градитељска и уметничка достигнућа старих цивилизација. Она показују велико знање, богатство, веровања и вештину народа који су их створили. Од свих античких чуда до данас је у потпуности сачувана само Кеопсова пирамида у Гизи, док су остала позната на основу историјских извора, описа и археолошких остатака.</textarea>
             <div class="center-image">
-                <img src="./slike/slika1.jpg" alt="Centralna slika">
+                <img src="./slike/slika1.jpg" alt="Централна слика">
             </div>
             <div class="small-image-list">
-                <div class="small-image"><img src="./slike/slika1.jpg" alt="slika 1" id="slika1"></div>
-                <div class="small-image"><img src="./slike/slika2.jpg" alt="slika 2" id="slika2"></div>
-                <div class="small-image"><img src="./slike/slika3.jpg" alt="slika 3" id="slika3"></div>
-                <div class="small-image"><img src="./slike/slika4.jpg" alt="slika 4" id="slika4"></div>
-                <div class="small-image"><img src="./slike/slika5.jpg" alt="slika 5" id="slika5"></div>
-                <div class="small-image"><img src="./slike/slika6.jpg" alt="slika 6" id="slika6"></div>
-                <div class="small-image"><img src="./slike/slika7.jpg" alt="slika 7" id="slika7"></div>
-                <div class="small-image"><img src="./slike/slika8.jpg" alt="slika 8" id="slika8"></div>
-                <div class="small-image"><img src="./slike/slika9.jpg" alt="slika 9" id="slika9"></div>
-                <div class="small-image"><img src="./slike/slika10.jpg" alt="slika 10" id="slika10"></div>
+                <div class="small-image"><img src="./slike/slika1.jpg" alt="слика 1" id="slika1"></div>
+                <div class="small-image"><img src="./slike/slika2.jpg" alt="слика 2" id="slika2"></div>
+                <div class="small-image"><img src="./slike/slika3.jpg" alt="слика 3" id="slika3"></div>
+                <div class="small-image"><img src="./slike/slika4.jpg" alt="слика 4" id="slika4"></div>
+                <div class="small-image"><img src="./slike/slika5.jpg" alt="слика 5" id="slika5"></div>
+                <div class="small-image"><img src="./slike/slika6.jpg" alt="слика 6" id="slika6"></div>
+                <div class="small-image"><img src="./slike/slika7.jpg" alt="слика 7" id="slika7"></div>
+                <div class="small-image"><img src="./slike/slika8.jpg" alt="слика 8" id="slika8"></div>
+                <div class="small-image"><img src="./slike/slika9.jpg" alt="слика 9" id="slika9"></div>
+                <div class="small-image"><img src="./slike/slika10.jpg" alt="слика 10" id="slika10"></div>
             </div>
         </div>
     </main>
