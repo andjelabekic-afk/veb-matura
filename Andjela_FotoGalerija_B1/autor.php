2cbd1af97561daef9834a9c3c2f859773f489409
Andjela <andjelabekic@gymko.edu.rs>
2026-06-04 17:27:06 +0200

    Update autor.php

--- Andjela_FotoGalerija_B1/autor.php
+++ Andjela_FotoGalerija_B1/autor.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,11 +7,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/global.css">
     <link rel="stylesheet" href="./css/autor.css">
-    <title>B1 - Foto Galerija | Autor</title>
+    <title>Б1 - Фото галерија | Аутор</title>
 </head>
 <body>
     <header>
-        <h1>Foto galerija - Autor</h1>
+        <h1>Фото галерија - Аутор</h1>
         <?php 
             $url = "autor";
             require "./components/nav.php";
@@ -20,15 +20,15 @@
     <main>
         <div class="info">
             <div class="name">
-                <h2>Andjela Bekic</h2>
+                <h2>Анђела Бекић</h2>
                 <h4>IV<span>3</span></h4>
             </div>
         </div>
         <div class="contact">
-    <p>Email: andjela.bekic2007@gmail.com</p>
-    <p>Telefon: +381 63 111 111</p>
-    <p>Adresa: Ulica 123, Kovacica, Srbija</p>
-</div>
+            <p>Имејл: andjela.bekic2007@gmail.com</p>
+            <p>Телефон: +381 63 111 111</p>
+            <p>Адреса: Улица 123, Ковачица, Србија</p>
+        </div>
     </main>
 </body>
 </html>
